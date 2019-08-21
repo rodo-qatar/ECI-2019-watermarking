7a215155e1490bc117b4426338a9bb957ac11b97
rodo-qatar <54189673+rodo-qatar@users.noreply.github.com>
2019-08-21 18:20:42 -0300

    Upload code for metric assessment

---
+++ config.py
@@ -0,0 +1,13 @@
+
+
+#Folder donde se encuentran las imagenes cover
+IMAGES_FOLDER = '/home/rodrigo/Documents/Escuelas/ECI_2019/Watermarking/Data' 
+
+#Folder donde se encuentran los textos a ocultar
+TEXT_FOLDER = '/home/rodrigo/Documents/Escuelas/ECI_2019/Watermarking' 
+
+#Definir comando de ejecución para el software de steganografia para crear la stego-imagen
+command_create = 'steghide embed -cf {} -ef {} -sf {} -Z -K -N -v -p 1234' 
+
+#Definir comando de ejecución para el software de steganografia para extraer de la stego-imagen el mensaje
+command_extract = 'steghide extract -sf {} -xf {} -p 1234'
